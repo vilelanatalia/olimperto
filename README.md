a330f5791d9f8416c4a4368a366fff7e4823852f
marcosmatasousa <marcosdamata2000@gmail.com>
2024-08-30 12:25:41 -0300

    pushing the project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# olimperto" 
+Quick tutorial for running the project:
+
+1. Download NodeJS 20.17.0 at `nodejs.org`
+
+2. At the project folder run `npm install`
+
+3. To run the project run `npm run dev` and open the URL shown in your terminal.
---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.div}>
+      <Button variant="contained">Button</Button>
+    </div>
+  );
+}
+
+export default App;
